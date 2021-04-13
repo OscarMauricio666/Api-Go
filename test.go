c992221f98b7794d8295eac3c80c85b65f16f866
OscarMauricio666 <mauriciorondonfajarodo@gmail.com>
2021-04-12 19:24:57 -0500

    refactorizado test.go

--- test.go
+++ test.go
@@ -11,14 +11,6 @@ func main() {
 	// models.Ping()
 	models.CreateTables()
 	models.CreateTicket("Prueba666", "óscar Mauricio Rondón fajardo", "Activo")
-	models.CreateTicket("333", "óscar Mauricio Rondón fajardo", "Activo")
-	models.CreateTicket("999", "óscar Mauricio Rondón fajardo", "Inactivo")
-
-	ticket666 := models.GetTicket(1)
-	fmt.Println(ticket666)
-
-	ticket999 := models.GetTickets()
-	fmt.Println(ticket999)
 
 	ticket := models.CreateTicket("Prueba666", "óscar Mauricio Rondón fajardo", "Activo")
 	fmt.Println(ticket)
